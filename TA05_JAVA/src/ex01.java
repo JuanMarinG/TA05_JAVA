cfecc386fbe0e61995c17784f950f0319a0a325c
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-07 12:39:02 +0200

    Ex01 subido

--- TA05_JAVA/src/ex01.java
+++ TA05_JAVA/src/ex01.java
@@ -3,7 +3,15 @@ public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a=6;
+		int b=4;
+		
+		if (a<b) {
+			System.out.println("La variable A es menor que B");
+			
+		}else System.out.println("La variable A es mayor que B");
+		
 	}
-
+	
 }
+

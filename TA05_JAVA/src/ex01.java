4bcc3a398df76da691bdaf2483ffd16758694cd4
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-11 01:08:53 +0200

    Primer Commit Ex03

--- TA05_JAVA/src/ex01.java
+++ TA05_JAVA/src/ex01.java
@@ -3,13 +3,16 @@ public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=6;
+		
+		//Declarando variables numericas con distinto valor.
+		
+		int a=10;
 		int b=4;
 		
-		if (a<b) {
-			System.out.println("La variable A es menor que B");
-			
-		}else System.out.println("La variable A es mayor que B");
+		//Usamos Math Max y Min entre las variables para que nos indique cual de las dos es menor o mayor.
+		
+		System.out.println("Entre el valor a(10) y el valor b(4), el mayor es: "+Math.max(a, b));
+		System.out.println("Entre el valor a(10) y el valor b (4), el menor es: "+Math.min(a, b));
 		
 	}
 	
--- TA05_JAVA/src/ex03.java
+++ TA05_JAVA/src/ex03.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-public class ex02 {
+
+import javax.swing.JOptionPane;
+public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
 		
 		System.out.print("Escribe tu nombre: ");
-		
 		String name = sc.nextLine();
-		sc.close();
 		
-		System.out.println("Bienvenido " +name.toUpperCase());
-		
+		JOptionPane.showMessageDialog(null, name);
 	}
 
 }
